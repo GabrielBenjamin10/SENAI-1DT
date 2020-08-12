7fa16e9c9f3b945ee8abe93ed8804a2f88ef03c9
Gabriel <gabriel.gaspargbg@gmail.com>
2020-08-11 21:10:39 -0300

    celular console poo

---
+++ atividade-poo-celular/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SS_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            celular iphone = new celular();
+            iphone.modelo = "iphone6s";
+            iphone.tamanho= 10f;
+            iphone.ligado = false;
+
+            if (iphone.ligado){
+                Console.WriteLine(iphone.Ligando());
+                Console.WriteLine(iphone.EnviarMsg());
+            }
+            else{
+                Console.WriteLine("O celular esta desligado");
+            }
+            
+        }
+    }
+}
