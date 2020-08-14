30ac1c06dfade94297b0e6089403f417852ff155
Gabriel <gabriel.gaspargbg@gmail.com>
2020-08-13 22:00:29 -0300

    Primeiro Projeto de POO

---
+++ Primeiro-POO/Lampada.cs
@@ -0,0 +1,15 @@
+namespace POO
+{
+    public class Lampada
+    {
+        public string modelo;
+        public string cor;
+
+
+        public string Acender()
+        {
+            return "Esta ligada";
+        }
+
+    }
+}
---
+++ Primeiro-POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lampada lampada = new Lampada();
+            Console.WriteLine(" Qual o modelo (LED ou FLuorescente)? ");            
+            lampada.modelo = Console.ReadLine();
+            Console.WriteLine("Qual a cor?");
+            lampada.cor = Console.ReadLine();
+            Console.WriteLine("A Lâmpada de "+ lampada.modelo );
+            Console.WriteLine("Cor " + lampada.cor);
+            Console.WriteLine(lampada.Acender());
+            
+          
+
+
+
+
+
+
+        }
+    }
+}
